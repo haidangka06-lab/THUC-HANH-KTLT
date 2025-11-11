9b5b1d15cdf86e6e18b1a7194421a9a773ed24f4
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:25:24 +0700

    Update cau5.py

--- Bai 5/cau5.py
+++ Bai 5/cau5.py
@@ -1,1 +1,9 @@
+class DaoChuoi:
+    def reverse_words(self, s):
+        words = s.split()
+        reversed_words = ' '.join(reversed(words))
+        return reversed_words
+s = 'hello .py'
+dao = DaoChuoi()
+print("Kết quả:", dao.reverse_words(s))
 
