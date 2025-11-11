c850dff783687b953aca0522791c63a3522ecd6e
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:26:51 +0700

    Update cau6.py

--- Bai 5/cau6.py
+++ Bai 5/cau6.py
@@ -1,1 +1,11 @@
+class StringProcessor:
+    def __init__(self):
+        self.s = ""
+    def get_String(self):
+        self.s = input("Nhập chuỗi: ")
+    def print_String(self):
+        print("Chuỗi in hoa:", self.s.upper())
+sp = StringProcessor()
+sp.get_String()
+sp.print_String()
 
