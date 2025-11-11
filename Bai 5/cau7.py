49d0e888796c1aa57b488a0bcb34da98addef4e5
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:28:53 +0700

    Update cau7.py

--- Bai 5/cau7.py
+++ Bai 5/cau7.py
@@ -1,1 +1,11 @@
+class Circle:
+    def __init__(self, r):
+        self.radius = r
+    def area(self):
+        return 3.14 * self.radius ** 2
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+c = Circle(5)
+print("Diện tích hình tròn:", c.area())
+print("Chu vi hình tròn:", c.perimeter())
 
