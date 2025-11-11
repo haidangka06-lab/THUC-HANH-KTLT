e67040afb552baeef5511823945515db9d338962
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:10:12 +0700

    Update cau8.py

--- Bài 4/cau8.py
+++ Bài 4/cau8.py
@@ -1,1 +1,14 @@
+from sequential_search import Sequential_Search
+n = int(input("Nhập số phần tử của danh sách: "))
+dlist = []
+for i in range(n):
+    value = int(input(f"Nhập phần tử thứ {i+1}: "))
+    dlist.append(value)
+# Nhập phần tử cần tìm
+item = int(input("Nhập phần tử cần tìm: ")
+found, index = Sequential_Search(dlist, item)
+if found:
+    print(f"Phần tử {item} được tìm thấy tại chỉ mục {index}")
+else:
+    print(f"Phần tử {item} không có trong danh sách.")
 
