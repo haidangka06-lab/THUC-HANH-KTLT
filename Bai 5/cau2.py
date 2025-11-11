32668f38cf87a7e29de45c01e6b440c2173ae31f
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:21:13 +0700

    Update cau2.py

--- Bai 5/cau2.py
+++ Bai 5/cau2.py
@@ -1,1 +1,10 @@
+class Hinhchunhat:
+    def __init__(self, dai, rong):
+        self.dai = dai
+        self.rong = rong
+
+    def dientich(self):
+        return self.dai * self.rong
+hcn = Hinhchunhat(5, 3)
+print("Diện tích hình chữ nhật:", hcn.dientich())
 
