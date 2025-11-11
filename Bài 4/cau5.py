adc408d837d5d822c5c7280de4d30faef20f896a
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 08:27:17 +0700

    Update cau5.py

--- Bài 4/cau5.py
+++ Bài 4/cau5.py
@@ -1,1 +1,9 @@
-
+def sort_list(lst):
+    """Tra ve danh sach da sap xep(tang dan)."""
+    return sorted(lst)
+def find_min_max(lst):
+    """Tra ve (min_value, max_value) cua lst"""
+    if not lst:
+        raise ValueError("Danh sach rong")
+    return min(lst), max(lst)    
+    
