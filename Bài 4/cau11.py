c24acdaa53b9cdc3cc816b12f3c5a4491a259131
haidangka06-lab <haidangka06@gmail.com>
2025-11-11 09:15:21 +0700

    Update cau11.py

--- Bài 4/cau11.py
+++ Bài 4/cau11.py
@@ -1,1 +1,14 @@
+import numpy as np
+students = [
+    ('James', 5, 48.5),
+    ('Nail', 6, 52.5),
+    ('Paul', 5, 42.1),
+    ('Pit', 5, 40.11)
+]
+dtype = [('name', 'U10'), ('class', int), ('height', float)]
+arr = np.array(students, dtype=dtype)
+sorted_arr = np.sort(arr, order=['class', 'height'])
+print("Kết quả sắp xếp:")
+print(sorted_arr)
+
 
