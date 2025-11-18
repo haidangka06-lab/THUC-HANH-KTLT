8ff73f4e5fcf6c11262cab843f7892c736c308ff
haidangka06-lab <haidangka06@gmail.com>
2025-11-18 10:25:01 +0700

    Update cau9.py

--- Bai 6/cau9.py
+++ Bai 6/cau9.py
@@ -1,1 +1,7 @@
+def copy_file(src, dest):
+    with open(src, "r", encoding="utf-8") as f1:
+        data = f1.read()
+    with open(dest, "w", encoding="utf-8") as f2:
+        f2.write(data)
+copy_file("test.txt", "copy_test.txt")
 
