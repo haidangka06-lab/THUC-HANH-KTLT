a58e8f0cc95d4d1284063dbe5f1487a62f744713
haidangka06-lab <haidangka06@gmail.com>
2025-11-18 10:01:35 +0700

    Update cau6.py

--- Bai 6/cau6.py
+++ Bai 6/cau6.py
@@ -1,1 +1,7 @@
+def file_read_from_tail(fname, nlines):
+    with open(fname, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines[-nlines:]:
+        print(line, end="")
+file_read_from_tail("test.txt", 2)
 
