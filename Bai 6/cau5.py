74b1f5c9759dcce050af09d92a174ce80d63f530
haidangka06-lab <haidangka06@gmail.com>
2025-11-18 09:59:56 +0700

    Update cau5.py

--- Bai 6/cau5.py
+++ Bai 6/cau5.py
@@ -1,1 +1,7 @@
-
+def file_append_and_show(fname):
+    with open(fname, "a", encoding="utf-8") as f:
+        f.write("Python Exercises\n")
+        f.write("Java Exercises\n")
+    with open(fname, "r", encoding="utf-8") as f:
+        print(f.read())
+file_append_and_show("abc.txt")
