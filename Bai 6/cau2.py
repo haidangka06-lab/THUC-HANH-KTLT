cc01e4787b0b7fb7c41e0a6ce58ee4d28448a35f
haidangka06-lab <haidangka06@gmail.com>
2025-11-18 09:41:14 +0700

    Update cau2.py

--- Bai 6/cau2.py
+++ Bai 6/cau2.py
@@ -1,1 +1,10 @@
-
+def count_file(fname):
+    with open(fname, "r", encoding="utf-8") as f:
+        text = f.read()
+    num_chars = len(text)
+    num_words = len(text.split())
+    num_lines = text.count("\n") + 1
+    print("Số ký tự:", num_chars)
+    print("Số từ:", num_words)
+    print("Số dòng:", num_lines)
+count_file("test.txt")
