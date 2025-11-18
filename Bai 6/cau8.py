02b4b2813aec35795084d6aaf1d3c49281e93ba9
haidangka06-lab <haidangka06@gmail.com>
2025-11-18 10:05:54 +0700

    Update cau8.py

--- Bai 6/cau8.py
+++ Bai 6/cau8.py
@@ -1,1 +1,6 @@
+def write_list_to_file(fname, mylist):
+    with open(fname, "w", encoding="utf-8") as f:
+        for item in mylist:
+            f.write(item + "\n")
+write_list_to_file("list.txt", ["Python", "Java", "C++", "PHP"])
 
