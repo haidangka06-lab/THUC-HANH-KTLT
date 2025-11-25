bc0b957a683ba7ee7c95259f32b4decbd123bc06
haidangka06-lab <haidangka06@gmail.com>
2025-11-25 08:06:23 +0700

    Update cau4.py

--- Bai 7/cau4.py
+++ Bai 7/cau4.py
@@ -1,1 +1,16 @@
+from tkinter import *
+window = Tk()
+window.title("Demo Tkinter")
+window.geometry('350x200')
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+btn = Button(window, 
+             text="Click Me", 
+             command=clicked,
+             bg="yellow", 
+             fg="blue")
+btn.grid(column=1, row=0)
+window.mainloop()
 
